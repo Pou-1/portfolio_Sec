8bfb7882512f358ac5578b6262af46d658ffba6f
Emily <cypr.jeux@gmail.com>
2024-12-24 03:01:21 +0100

    ✨ changing to vite

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="w-screen min-h-screen flex">
+      
+      <div className="bg-red-400 w-44 h-40"></div>
+      Hii
+      <div className="bg-red-400 w-1/2 h-1/2"></div>
+      </div>
+  );
+};
+
+export default App;
