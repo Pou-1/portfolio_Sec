e89d31a44bfea45904e340bfba09b8047734048a
Emily <cypr.jeux@gmail.com>
2025-02-10 03:38:51 +0100

    feat: animation and test Test Pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,12 @@ import "../App.css";
 
 const App: React.FC = () => {
 	return (
-		<div className="w-screen min-h-screen h-screen flex relative bg-customDark"></div>
+		<div className="w-screen flex-center min-h-screen h-screen flex relative bg-customDark">
+			<div className="flex flex-wrap gap-5">
+		
+
+			</div>
+		</div>
 	);
 };
 
