3f7e0a70b77791abc976a796419cea07285b078d
Emily <cypr.jeux@gmail.com>
2025-03-30 19:17:56 +0200

    Flags

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,5 +1,7 @@
 import { useTranslation } from "react-i18next";
 import Button from "./Button";
+import FrenchFlagLogo from "../assets/logo/FRFlagLogo";
+import UKFlagLogo from "../assets/logo/UKFlagLogo";
 
 type TopBarProps = {
   activeIndex: number;
@@ -11,16 +13,22 @@ const TopBar: React.FC<TopBarProps> = ({ activeIndex }) => {
 
   return (
     <div className="flex flex-col justify-center p-2 z-50 w-full absolute top-2 flex-center">
-      <div className="rounded-full w-full px-5 py-1 gap-2 flex justify-between overflow-hidden">
+      <div className="rounded-full bg-red-100/40 backdrop-blur w-full px-5 py-1 gap-2 flex items-center justify-between overflow-hidden">
         <button
           onClick={() => i18n.changeLanguage(i18n.language === "fr" ? "en" : "fr")}
           className="p-2 bg-red-100 text-red-800 hover:bg-red-200 trans-fast rounded"
         >
-          {i18n.language === "fr" ? "English" : "français"}
+          {i18n.language === "fr" ? (
+            <UKFlagLogo/>
+          ) : (
+            <FrenchFlagLogo/>
+          )}
         </button>
-        {activeIndex == 0 && (sectionsNames[0])}
-        {activeIndex > 0 && activeIndex < 5 && (sectionsNames[1])}
-        {activeIndex > 4 && (sectionsNames[2])}
+        <p>
+          {activeIndex == 0 && (sectionsNames[0])}
+          {activeIndex > 0 && activeIndex < 5 && (sectionsNames[1])}
+          {activeIndex > 4 && (sectionsNames[2])}
+        </p>
         <Button text="Emily D'Harambure"/>
         <Button text="Contact Me"/>
       </div>
