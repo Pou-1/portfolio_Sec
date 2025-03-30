c5022022f7663ed56580bd5dc8fe726be2c149a8
Emily <cypr.jeux@gmail.com>
2025-03-30 15:48:51 +0200

    fix unused

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,10 @@
 import "../App.css";
 import "../styles/test.css";
 import "../styles/home.css";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ScrollSnapControl from "../components/ScrollSnapControl";
 
 const App: React.FC = () => {
-  const [scrollY, setScrollY] = useState<number>(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <>
       <ScrollSnapControl/>
