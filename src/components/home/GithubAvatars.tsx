6b8ab5b8ec7330574479d3253d784fc9a9e4d923
Emily <cypr.jeux@gmail.com>
2025-03-31 00:42:59 +0200

    navbar and github imgs

--- src/components/home/GithubAvatars.tsx
+++ src/components/home/GithubAvatars.tsx
@@ -1,37 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 type AvatarClusterProps = {
   usernames: string[];
 };
 
 const GithubAvatars: React.FC<AvatarClusterProps> = ({ usernames }) => {
-  const [avatars, setAvatars] = useState<string[]>([]);
-
-  useEffect(() => {
-    const fetchAvatars = async () => {
-      try {
-        const responses = await Promise.all(
-          usernames.map((username) =>
-            fetch(`https://api.github.com/users/${username}`).then((res) =>
-              res.json()
-            )
-          )
-        );
-        setAvatars(responses.map((user) => user.avatar_url));
-      } catch (error) {
-        console.error("Error fetching GitHub avatars:", error);
-      }
-    };
-    fetchAvatars();
-  }, [usernames]);
 
   return (
     <div className="flex -space-x-4">
-      {avatars.map((avatar, index) => (
+      {usernames.map((username, index) => (
         <img
           key={index}
-          src={avatar}
-          alt={`Avatar ${index}`}
+          src={`https://github.com/${username}.png`}
+          alt={`Avatar of ${username}`}
           className="w-12 h-12 rounded-full border-2 border-white shadow-lg"
         />
       ))}
