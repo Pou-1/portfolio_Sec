a6f204c78a777271975b1e6f8897505a76374853
Emily <cypr.jeux@gmail.com>
2025-03-31 02:09:33 +0200

    💄 Logos and bg

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,38 +1,47 @@
 import { useTranslation } from "react-i18next";
 import FrenchFlagLogo from "../assets/logo/FRFlagLogo";
 import UKFlagLogo from "../assets/logo/UKFlagLogo";
+import { t } from "i18next";
 
 type TopBarProps = {
-  activeIndex: number;
+	activeIndex: number;
 };
 
 const TopBar: React.FC<TopBarProps> = ({ activeIndex }) => {
-  const { i18n } = useTranslation();
-  const sectionsNames = ["Who am i", "Works", "About"]
+	const { i18n } = useTranslation();
+	const sectionsNames = ["Who am i", "Works", "About"];
 
-  return (
-    <div className="flex flex-col justify-center p-2 z-50 w-full absolute top-2 flex-center">
-      <div className="rounded-full bg-red-100/40 backdrop-blur w-full gap-2 flex items-center justify-between overflow-hidden">
-        <button
-          onClick={() => i18n.changeLanguage(i18n.language === "fr" ? "en" : "fr")}
-          className="p-4 text-red-800 hover:bg-red-200 trans-fast rounded"
-        >
-          {i18n.language === "fr" ? (
-            <UKFlagLogo/>
-          ) : (
-            <FrenchFlagLogo/>
-          )}
-        </button>
-        <p className="text-xl text-white py-2 rounded-md w-40 flex-center px-4 bg-[#C522FB] shadow-md">
-          {activeIndex == 0 && (sectionsNames[0])}
-          {activeIndex > 0 && activeIndex < 5 && (sectionsNames[1] + ' - ' + activeIndex)}
-          {activeIndex > 4 && (sectionsNames[2])}
-        </p>
-        <p>Emily D'Harambure</p>
-        <button className="rounded-full text-white py-2 px-6 bg-[#F32E66] hover:bg-[#fd30e9] trans-fast m-2">Contact Me</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col justify-center p-2 z-50 w-full absolute top-2 flex-center">
+			<div className="rounded-full bg-red-100/40 backdrop-blur w-full gap-2 flex items-center justify-between">
+				<button
+					onClick={() =>
+						i18n.changeLanguage(
+							i18n.language === "fr" ? "en" : "fr"
+						)
+					}
+					className="p-4 text-red-800 hover:bg-red-200 trans-fast rounded-l-full"
+				>
+					{i18n.language === "fr" ? (
+						<UKFlagLogo />
+					) : (
+						<FrenchFlagLogo />
+					)}
+				</button>
+				<p className="text-xl text-white py-2 rounded-md w-40 flex-center px-4 bg-[#C522FB] shadow-md">
+					{activeIndex == 0 && sectionsNames[0]}
+					{activeIndex > 0 &&
+						activeIndex < 5 &&
+						sectionsNames[1] + " - " + activeIndex}
+					{activeIndex > 4 && sectionsNames[2]}
+				</p>
+				<p>Emily D'Harambure</p>
+				<button className="rounded-full text-white py-2 px-6 bg-[#F32E66] hover:bg-[#fd30e9] trans-fast m-2">
+					{t("contactMe")}
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default TopBar;
--- src/components/home/FirstCardDiv.tsx
+++ src/components/home/FirstCardDiv.tsx
@@ -3,86 +3,97 @@ import ArrowLogo from "../../assets/logo/ArrowLogo";
 import GithubLogo from "../../assets/logo/GithubLogo";
 import LinkedinLogo from "../../assets/logo/LinkedinLogo";
 import { useTranslation } from "react-i18next";
+import MailLogo from "../../assets/logo/MailLogo";
+import CVLogo from "../../assets/logo/CVLogo";
 
 type AnimatedCardProps = {
-  scrollY: number;
+	scrollY: number;
 };
 
 const FirstCardDiv: React.FC<AnimatedCardProps> = () => {
-  const { t } = useTranslation();
-  return (
-    <>
-      <div className="w-full h-screen relative flex-center flex-col">
-        {/* Cercles avec un effet parallaxe plus fort */}
+	const { t } = useTranslation();
+	return (
+		<>
+			<div className="w-full h-screen relative flex-center flex-col">
+				{/* Cercles avec un effet parallaxe plus fort */}
 
-        {/* Contenu principal */}
-        <div className="mt-20 relative mr-4 ml-4 p-5 max-w-3xl mx-auto">
-          {/* Cercle en haut à gauche */}
-          <div className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 w-20 h-20 md:w-32 md:h-32 rounded-full z-20 bg-[#F32E66]" />
-          {/* Cercle en bas à droite */}
-          <div className="absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2 w-16 h-16 md:w-28 md:h-28 rounded-full bg-[#C522FB]" />
+				{/* Contenu principal */}
+				<div className="mt-20 relative mr-4 ml-4 p-5 max-w-3xl mx-auto">
+					{/* Cercle en haut à gauche */}
+					<div className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 w-20 h-20 md:w-32 md:h-32 rounded-full z-20 bg-[#F32E66]" />
+					{/* Cercle en bas à droite */}
+					<div className="absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2 w-16 h-16 md:w-28 md:h-28 rounded-full bg-[#C522FB]" />
 
-          <div className="bg-white/50 z-30 relative backdrop-blur-xl rounded-xl shadow-lg w-full h-full flex-col md:flex-row flex">
-            <div className="w-full flex-center p-5">
-              <img
-                src={Picrew}
-                alt={"A Profil picture of me"}
-                className="rounded-full md:w-full w-64 aspect-square"
-              />
-            </div>
-            <div className="p-6 flex flex-col justify-center">
-              <div className="mb-5">
-                <h1>{t("welcome")}</h1>
-                <h1 className="font-bold text-3xl text-[#F32E66]">
-                  {t("name")}
-                </h1>
-              </div>
-              <p className="text-sm">{t("study")}</p>
-              <p className="text-sm">{t("currently")}</p>
-              <div className="mt-10 flex gap-5">
-                <button
-                  className="p-4 bg-white hover:bg-red-100 trans-fast cursor-pointer shadow-md rounded-xl"
-                  onClick={() =>
-                    window.open("https://www.linkedin.com", "_blank")
-                  }
-                >
-                  <LinkedinLogo size={30} />
-                </button>
-                <button
-                  className="p-4 bg-white hover:bg-red-100 trans-fast cursor-pointer shadow-md rounded-xl"
-                  onClick={() =>
-                    window.open("https://github.com/Pou-1", "_blank")
-                  }
-                >
-                  <GithubLogo size={30} />
-                </button>
-                <button
-                  className="p-4 bg-white hover:bg-red-100 trans-fast cursor-pointer shadow-md rounded-xl"
-                  onClick={() =>
-                    (window.location.href =
-                      "mailto:cyprien.dharambure@gmail.com")
-                  }
-                >
-                  <GithubLogo size={30} />
-                </button>
-                <button
-                  className="p-4 bg-white hover:bg-red-100 trans-fast cursor-pointer shadow-md rounded-xl"
-                  onClick={() =>
-                    window.open("/CV_DELAPOEZEDHARAMBURE_Cyprien.pdf", "_blank")
-                  }
-                >
-                  <GithubLogo size={30} />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="mt-20 animate-bounce">
-          <ArrowLogo className="rotate-90" size={40} />
-        </div>
-      </div>
-    </>
-  );
+					<div className="bg-white/50 z-30 relative backdrop-blur-xl rounded-xl shadow-lg w-full h-full flex-col md:flex-row flex">
+						<div className="w-full flex-center p-5">
+							<img
+								src={Picrew}
+								alt={"A Profil picture of me"}
+								className="rounded-full md:w-full w-64 aspect-square"
+							/>
+						</div>
+						<div className="p-6 flex flex-col justify-center">
+							<div className="mb-5">
+								<h1>{t("welcome")}</h1>
+								<h1 className="font-bold text-3xl text-[#F32E66]">
+									{t("name")}
+								</h1>
+							</div>
+							<p className="text-sm">{t("study")}</p>
+							<p className="text-sm">{t("currently")}</p>
+							<div className="mt-10 flex gap-5">
+								<button
+									className="p-4 bg-white hover:bg-blue-100 trans-fast cursor-pointer shadow-md rounded-xl"
+									onClick={() =>
+										window.open(
+											"https://www.linkedin.com",
+											"_blank"
+										)
+									}
+								>
+									<LinkedinLogo size={30} />
+								</button>
+								<button
+									className="p-4 bg-white hover:bg-black/10 trans-fast cursor-pointer shadow-md rounded-xl"
+									onClick={() =>
+										window.open(
+											"https://github.com/Pou-1",
+											"_blank"
+										)
+									}
+								>
+									<GithubLogo size={30} />
+								</button>
+								<button
+									className="p-4 bg-white hover:bg-violet-100 text-violet-700 trans-fast cursor-pointer shadow-md rounded-xl"
+									onClick={() =>
+										(window.location.href =
+											"mailto:cyprien.dharambure@gmail.com")
+									}
+								>
+									<MailLogo size={30} />
+								</button>
+								<button
+									className="p-4 bg-white text-red-700 hover:bg-red-100 trans-fast cursor-pointer shadow-md rounded-xl"
+									onClick={() =>
+										window.open(
+											"/CV_DELAPOEZEDHARAMBURE_Cyprien.pdf",
+											"_blank"
+										)
+									}
+								>
+									<CVLogo size={30} />
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="mt-20 animate-bounce">
+					<ArrowLogo className="rotate-90" size={40} />
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default FirstCardDiv;
